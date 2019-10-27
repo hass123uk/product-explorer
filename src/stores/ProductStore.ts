25abeca85fed2ce37c1d2995c1d9c6b0e6223b76
Hassan <hass123uk@hotmail.com>
2019-10-27 19:29:14 +0100

    Add isLoading to Product store to help with testing

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -22,23 +22,38 @@ export const ProductModel = types.model('ProductModel', {
 });
 
 export const ProductStore = types.model('ProductStore', {
+    isLoading: true,
     products: types.optional(types.array(ProductModel), [])
-}).actions(self => {
-    const loadData = flow(function* load() {
-        const products = yield loadProducts();
-        applySnapshot(self.products, products);
-    });
-    const deleteData = flow(function* remove(product: any) {
+}).actions(self => ({
+    markLoading(loading: boolean) {
+        self.isLoading = loading
+    }
+
+})).actions(self => ({
+    loadData: flow(function* load() {
+        try {
+            const products = yield loadProducts();
+            if (products) {
+                applySnapshot(self.products, products);
+            }
+            self.markLoading(false);
+        } catch (error) {
+            console.error(error);
+        }
+    })
+})).actions(self => ({
+    deleteData: flow(function* remove(product: any) {
         yield deleteProduct(product.id);
         //Load the data again after deleting to ensure we are sync with server.
-        loadData();
-    });
+        self.loadData();
+    })
+})).actions(self => {
     return {
         afterCreate() {
-            loadData();
+            self.loadData();
         },
         remove(product: any) {
-            deleteData(product);
+            self.deleteData(product);
             //Delete product from store directly to update UI quickly
             destroy(product);
         }
